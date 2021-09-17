6da8e677bcb6774993cf72e5b6a857fdd02d7c43
sivaji <you@example.com>
2021-09-17 20:16:55 +0530

    It Tells How to read Dictionary using pandas DataFrame

---
+++ Read_dictionart.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+student_marks={
+    'mid':['25','26','29','21','27'],'mid2':['25','17','25','24','29'],'class_T1':['10','8','9','15','19'],'class_T1':['10','8','9','15','19']
+}
+df=pd.DataFrame(student_marks)
+print(df)
